cd3cfd0f76825a90a604b37672405c0541a5afb9
alexsokolowska <alexs@physik.uzh.ch>
2017-04-04 12:29:58 +0200

    fixed issues when dealing with large numbers

--- rename.py
+++ rename.py
@@ -5,7 +5,7 @@ rename.py
 This program renames simulation files. If you output every Xth run of your simulation, i.e. the snapshot numbers follow a sequence with a step X (start, start+X, start+2X, ...), with this program you can change the ordering to be an array of integers with a step X=1. 
 
 """
-
+#start = 292, end = 196, new_start=97, step = 4
 import os, glob, numpy as np
 from sys import argv
 try:
@@ -26,13 +26,17 @@ print "Number of outputs %d" %N
 vec = np.arange(new_start, new_start + N)
 
 for i in range(N):
-	if (start+N*i<10):
+	if (start+i*step<10):
 		string = "0000"+str(start+i*step)
-	elif (start+N*i<100):
+	elif (start+i*step<100):
 		string = "000"+str(start+i*step)
-	else:
+	elif (start+i*step<1000):
 		string = "00"+str(start+i*step)
-
+	elif (start+i*step<1e4):
+		string = "0"+str(start+i*step)
+	else:
+		string = str(start+i*step)
+	
 	path = sim_name + '.' + string + '*'
 	LoR = glob.glob(path) #list of runs with the same old number
 	print LoR
@@ -41,8 +45,12 @@ for i in range(N):
 		string2 = "0000"+str(vec[i])
 	elif vec[i]<100:
 		string2 = "000"+str(vec[i])
-	else:
+	elif vec[i]<1000:
 		string2 = "00"+str(vec[i])
+	elif vec[i]<1e4:
+		string2 = "0"+str(vec[i])
+	else:
+		string2 = str(vec[i])
 	for j in range(len(LoR)):
 		old_name = LoR[j]
 		new_name = sim_name + '.' + string2+LoR[j][len(sim_name)+6:]
